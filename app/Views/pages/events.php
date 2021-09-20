f314c1167b8b0991634fc69317a2c8f10f881caf
dev-kiplangat <kiplangatsgt@gmail.com>
2021-09-20 07:16:05 +0300

    *

--- app/Views/pages/events.php
+++ app/Views/pages/events.php
@@ -1,6 +1,17 @@
 <?php include(APPPATH.'Views/templates/side-nav.php'); ?>
+<?php include(APPPATH.'Views/templates/lunar-css.php'); ?>
+
+
 
 <div class="container-fluid">
+    <div class="d-sm-flex justify-content-between align-items-center mb-4">
+        <h3 class="text-dark mb-0">Behold the term calender Events</h3><a class="btn btn-primary btn-sm d-none d-sm-inline-block" role="button" id="opener" href="#"><i class="fas fa-plus fa-sm text-white-50"></i>&nbsp; &nbsp;Add Event</a>
+    </div>
+
+    <!-- create events modal -->
+    <?php include(APPPATH.'Views/modals/event-create.php'); ?>
+
+
     <div class="row">
         <div class="col">
             <div class="card shadow mb-4">
@@ -24,14 +35,37 @@
 
  <script>
 
-      document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
-        });
-        calendar.render();
-      });
+document.addEventListener('DOMContentLoaded', function() {
+var calendarEl = document.getElementById('calendar');
+var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    initialDate: '2021-09-19',
+    headerToolbar: {
+        left: 'prev, next, today',
+        center: 'title',
+        right: 'dayGridMonth, timeGridWeek, timeGridDay'
+    },
+    events: [
+    {
+        title: 'All day event',
+        start: '2021-09-22'
+    },
+    {
+        title: 'Long Event',
+        start: '2021-09-27',
+        end: '2021-09-29'
+    },
+    {
+        title: 'All day event',
+        start: '2021-09-22T16:30:00',
 
-    </script>
+    }
+]
+});
+calendar.render();
+});
 
+</script>
+
+<?php include(APPPATH.'Views/templates/lunar-scripts.php'); ?>◘◘
 <?php include(APPPATH.'Views/templates/footer-section.php'); ?>
