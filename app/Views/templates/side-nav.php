14ff15cec078e801324edf42a63fcf1290b29106
dev-kiplangat <kiplangatsgt@gmail.com>
2021-10-03 07:01:59 +0300

    add class route

--- app/Views/templates/side-nav.php
+++ app/Views/templates/side-nav.php
@@ -43,7 +43,9 @@
                             </div>
                         </div>
                     </li>
+                    <li class="nav-item"><a class="nav-link <?php if($path === 'admin/classes') echo 'active' ?>" href="/admin/classes"><i class="fa fa-building"></i><span>&nbsp;classes</span></a></li>
                     <li class="nav-item"><a class="nav-link <?php if($path === 'admin/subjects') echo 'active' ?>" href="/admin/subjects"><i class="fa fa-copy"></i><span>&nbsp;Subjects</span></a></li>
+
                     <li class="nav-item"><a class="nav-link <?php if($path === 'admin/schedule') echo 'active' ?>" href="/admin/schedule"><i class="fa fa-copy"></i><span>&nbsp;Schedule(TT)</span></a></li>
                     <li class="nav-item"><a class="nav-link <?php if($path === 'admin/notices') echo 'active' ?>" href="/admin/notices"><i class="fas fa-file-alt"></i><span>&nbsp;Notices</span></a></li>
                     <li class="nav-item"><a class="nav-link <?php if($path === 'admin/messages') echo 'active' ?>" href="/admin/messages"><i class="fas fa-envelope"></i><span>&nbsp;Messages</span></a></li>
