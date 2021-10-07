97e2dc377118375811b9ecc2027c7404a35957a8
dev-kiplangat <kiplangatsgt@gmail.com>
2021-10-07 07:59:40 +0300

    custom view helper fun

---
+++ app/Helpers/state_utils_helper.php
@@ -0,0 +1,15 @@
+<?php
+
+// type of alerts
+// 1. Account reset
+// 2. print report
+// 3. verification
+// 4. system notification
+
+    function alert_action_btn() {
+            return "<a class='btn btn-primary btn-sm' href='..'>Authorize</a>";
+    }
+
+
+
+?>
