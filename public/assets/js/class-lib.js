79df76ffbc97e50f5cfbbbb3ff57c9f3e24cd922
dev-kiplangat <kiplangatsgt@gmail.com>
2021-10-11 14:18:59 +0300

    edit-schedule modal dynamic

--- public/assets/js/class-lib.js
+++ public/assets/js/class-lib.js
@@ -41,3 +41,65 @@ $('[data-dismiss]').click(function () {
      $(".modal:not(.auto-off)").modal("hide");
 })
 
+// 
+$('[data-classviewid]').click(function () {
+    const schedule_id = $(this).data('classviewid')
+
+     // pop up modal
+    $('#edit-Class').modal('show')
+
+    // make a get request
+    $.ajax({
+        type: 'get',
+        url: `${base_url}/admin/api/schedules?_id=${schedule_id}`,
+        format: 'json',
+        error: function (err) {
+            console.log(err.statusText)
+        },
+        success: function (response, responseText) {
+            if (responseText == 'success') {
+                
+                if (Array.isArray(response)) {
+                    response.forEach(el => {
+                        mapEditModal(el)
+                    })
+                }
+
+                else {
+                    for (const el in response) {
+                        mapEditModal(response[el])
+                    }
+                }
+               
+            }
+            else {
+                // launch failure modal
+            }
+        }
+    })
+})
+
+// map values to edit modal
+
+function mapEditModal(data) {
+    // pick the nodes
+    $teacher = $('#teacher_id')
+    $class = $('#class_opt')
+    $stream = $('#stream_opt')
+    $subject = $('#subject_opt')
+    $day = $('#day_opt')
+    // fields
+    const startTime = document.querySelector('#startTime')
+    const endTime = document.querySelector('#endTime')
+    
+    $teacher[0][0].innerText = data.t_name
+    $class[0][0].innerText = data.class_id
+    $stream[0][0].innerText = data.stream
+    $subject[0][0].innerText = data.name
+    $day[0][0].innerText = data.day
+
+    startTime.value = data.start
+    endTime.value = data.end
+
+    
+}
